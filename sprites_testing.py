bdf73ad3a6fd612a822fb6a12b7298c2b4a9c29c
Cosmopolux <cosmo.bordier@gmail.com>
2023-04-06 15:22:46 +0200

    Create sprites_testing.py

---
+++ sprites_testing.py
@@ -0,0 +1,39 @@
+import pygame, sys
+
+class Cursor(pygame.sprite.Sprite):
+    def __init__(self, picture_path):
+        super().__init__()
+        self.image = pygame.image.load(picture_path)
+        self.rect = self.image.get_rect()
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+
+# Genral Setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Game Screen
+S_WIDTH = 900
+S_HIGHT = 500
+FPS = 60
+screen = pygame.display.set_mode((S_WIDTH,S_HIGHT))
+white = [255, 255, 255]
+pygame.mouse.set_visible(False)
+
+# Cursor
+cursor = Cursor("Sprites/cursor.png")
+cursor_group =pygame.sprite.Group()
+cursor_group.add(cursor)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
+    screen.fill(white)
+    cursor_group.draw(screen)
+    cursor_group.update()
+    clock.tick(FPS)
