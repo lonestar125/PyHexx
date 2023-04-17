f78fef8314451d8ae23fd0ba26e3eae4b6b7a46f
lonestar125 <a.mitton24@gmail.com>
2023-04-17 10:53:02 +0200

    added menu, fixed no more moves not ending game

--- hexxagon.py
+++ hexxagon.py
@@ -10,15 +10,6 @@ width, height = 800, 600
 global screen
 screen = pygame.display.set_mode((width, height))
 
-# Sprites
-HEX = "Sprites/resized_HEX.png"
-HEX_P1 = "Sprites/HEX_p1.png"
-HEX_P2 = "Sprites/HEX_p2.png"
-HEX_H = "Sprites/HEX_hidden.png"
-HEX_J = "Sprites/HEX_jumpable.png"
-HEX_C = "Sprites/HEX_cloneable.png"
-
-
 class Tile(pygame.sprite.Sprite):
 	def __init__(self, grid_el):
 		super(Tile, self).__init__()
@@ -30,13 +21,13 @@ class Tile(pygame.sprite.Sprite):
 
 	def update(self, grid_el):
 		if grid_el["status"] == 0:
-			self.image = pygame.image.load(HEX).convert_alpha()
+			self.image = pygame.image.load("Sprites/empty.png").convert_alpha()
 		if grid_el["status"] == 1:
-			self.image = pygame.image.load(HEX_P1).convert_alpha()
+			self.image = pygame.image.load("Sprites/red.png").convert_alpha()
 		if grid_el["status"] == 2:
-			self.image = pygame.image.load(HEX_P2).convert_alpha()
+			self.image = pygame.image.load("Sprites/blue.png").convert_alpha()
 		if grid_el["status"] == -1:
-			self.image = pygame.image.load(HEX_H).convert_alpha()
+			self.image = pygame.image.load("Sprites/hidden.png").convert_alpha()
 		
 	
 
@@ -45,20 +36,22 @@ def create_board():
 	"""
 	Create the hexxagon board.
 	"""
-	x_size = 36
-	y_size = 50
+	x_size = 38
+	y_size = 45
 	# grid: coord x, coord y, status, outlined, tile (the tile is only available for the grid items that are not out of bounds)
 	# state: -1 for empty, 0 for activated, 1 for player 1, 2 for player 2
 	# outlined: 0 for not outlined, 1 for clonable, 2 for jumpable
 	grid = [[{"x": x*x_size, "y": y*y_size, "status": -1, "outline": 0} for x in range(9)] for y in range(9)]
 	
-	margin = 238
+	x_margin = 225
+	y_margin = 50
 	# offset every second row by 25 on the y
 	for line in grid:
 		for el in line:
 			if int(el["x"] /x_size) % 2 == 0:
-				el["y"] += 25
-			el["x"] += margin
+				el["y"] += 23
+			el["x"] += x_margin
+			el["y"] += y_margin
 
 	activated = [[2, 7], [2, 8], [1, 8], [1, 9], [0, 9], [1, 9], [1, 8], [2, 8], [2, 7]]
 
@@ -162,15 +155,37 @@ def check_victory(score):
 			return 3 #tie
 	return None #no current winner
 
+def moves_left(current_player):
+	moves = []
+	for line in grid:
+		for el in line:
+			x, y = line.index(el), grid.index(line)
+			for el in even_n1 if x % 2 == 0 else odd_n1:
+				try: 
+					if grid[y + el[1]][x + el[0]]["status"] == 0 and (0 <= (y + el[1]) <= 8) and (0 <= (x + el[0]) <= 8):
+						moves.append(1)
+				except IndexError: # not shit code tkt
+					pass
+			if moves == []:
+				print("no more moves")
+				return False
+	return True
+
 def end_turn(cloneable, jumpable, current_player):
+	global in_game
+	global in_menu
 	clear_outlines()
 	cloneable = []
 	jumpable = []
 	score = get_score()
 	print(f"score: {score}")
 	current_player = abs(current_player - 3)
-	if check_victory(score) != None:
+	if check_victory(score) != None or moves_left(current_player): #order of these matters as the victory check has to come first if not a winning player may not play the last move and lose
 		print("Victory not fully implemented")
+		draw() #displays the last move
+		pygame.display.flip() #pushes update to screen for last move
+		in_game = False
+		in_menu = True
 		global grid
 		global group
 		group.empty()
@@ -210,7 +225,12 @@ def has_valid_path(x, y, visited):
 		return False
 
 
-def board_editor(editing_board):
+def board_editor():
+	global in_board_editor
+	global in_menu
+	screen.fill((24, 24, 24))
+	menu_rect = render_text("MENU", 100, 500, color=(241, 232, 205), size=50)
+
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit() # Opposite of pygame.init
@@ -225,19 +245,25 @@ def board_editor(editing_board):
 						if el["status"] != -1:
 							if not has_valid_path(line.index(el), grid.index(line), []):
 								print("invalid board")
-								return editing_board
+								return
 				
 				#check that both players have at least one tile
 				score = get_score()
 				if score[0] == 0 or score[1] == 0:
 					print("invalid board")
-					return editing_board
+					return
 				
-				editing_board = False
-				return editing_board
+				in_board_editor = False
+				in_menu = True
+				return
 
 		if event.type == MOUSEBUTTONDOWN and event.button == 1: 
 			x,y = event.pos
+
+			if menu_rect.collidepoint(x,y):
+				in_board_editor = False
+				in_menu = True
+
 			for line in grid:
 				for el in line:		
 					
@@ -265,14 +291,17 @@ def board_editor(editing_board):
 							if line.index(el) == 4 and grid.index(line) == 4: # the center tile can not be -1
 								el["status"] = 0
 								el["tile"].update(el)
-								return editing_board
+								return
 							el["status"] = -1
 							el["tile"].update(el)
-	return editing_board
 
 
 
-def update(cloneable, jumpable, selected_tile, current_player, score):
+def game(cloneable, jumpable, selected_tile, current_player, score):
+	global in_game
+	global in_menu
+	screen.fill((24, 24, 24))
+	menu_rect = render_text("MENU", 100, 500, color=(241, 232, 205), size=50)
 	# Go through events that are passed to the script by the window.
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -284,6 +313,10 @@ def update(cloneable, jumpable, selected_tile, current_player, score):
 		if event.type == MOUSEBUTTONDOWN and event.button == 1: 
 
 			x,y = event.pos
+
+			if menu_rect.collidepoint(x,y):
+				in_game = False
+				in_menu = True
 
 			for line in grid:
 				for el in line:
@@ -325,33 +358,80 @@ def update(cloneable, jumpable, selected_tile, current_player, score):
 
 def draw_outlines(cloneable, jumpable):
 	for el in cloneable:
-		img_i = pygame.image.load(HEX_C).convert_alpha()
+		img_i = pygame.image.load("Sprites/clone.png").convert_alpha()
 		img = img_i.get_rect()
 		img.x = el["x"]
-		img.y = el["y"]
+		img.y = el["y"] - 1
 		screen.blit(img_i, (img.x, img.y))
 	
 	for el in jumpable:
-		img_i = pygame.image.load(HEX_J).convert_alpha()
+		img_i = pygame.image.load("Sprites/jump.png").convert_alpha()
 		img = img_i.get_rect()
 		img.x = el["x"]
-		img.y = el["y"]
+		img.y = el["y"] - 1
 		screen.blit(img_i, (img.x, img.y))
+
+def render_text(text, x, y, color=(0, 0, 0), size=32, border=False):
+	'''
+	fenetre pygame, str, int, int -> None
+	options: color=(int,int,int), size=int, border=bool
+	affiche du texte a la position x, y de taille size et de couleur color
+	'''
+	font = pygame.font.Font('freesansbold.ttf', size)
+	text = font.render(text, True, color)
+	textRect = text.get_rect(center=(x,y))
+	#draw a box around text if the border option is set to True
+	if border:
+		pygame.draw.rect(screen, (0, 0, 0), (textRect.x-5, textRect.y-5, textRect.width+10, textRect.height+10), 5)
+	screen.blit(text, textRect)
+	return textRect
 	
+def menu():
+	global in_board_editor
+	global in_game
+	global in_info
+	global in_menu
+
+	screen.fill((24, 24, 24))
+	render_text("HEXXAGON", 400, 100, color=(241, 232, 205), size=50)
+	info_rect = render_text("INFO", 400, 300, color=(241, 232, 205), size=50)
+	board_rect = render_text("BOARD EDITOR", 400, 400, color=(241, 232, 205), size=50)
+	play_rect = render_text("PLAY", 400, 500, color=(241, 232, 205), size=50)
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit() # Opposite of pygame.init
+			sys.exit()
+		if event.type == MOUSEBUTTONDOWN and event.button == 1:
+			x,y = event.pos
+			if play_rect.collidepoint(x,y):
+				in_menu = False
+				in_game = True
+			elif info_rect.collidepoint(x,y):
+				in_menu = False
+				in_info = True
+			elif board_rect.collidepoint(x,y):
+				in_menu = False
+				in_board_editor = True
+				
 
 def draw():
 	"""
 	Draw things to the window. Called once per frame.
 	"""
-	screen.fill((0, 0, 0)) # Fill the screen with black.
+	global in_game
+	global in_board_editor
+	global in_menu
 	group.draw(screen) # draw tile sprites from the Tile class
 	
+
  
 def main():
 	pygame.init()
+	#pygame.font.init() #necessary for text rendering
 	global group #pygame.sprite.Group object, contains all active tiles
 	global grid
-	grid, group = create_board()
+	global total_tiles
 
 	# neighboring tile deltas [x, y], these are added to a tile to find its neighbors, global because they will not be modified in the rest of the code
 	global even_n1
@@ -364,30 +444,50 @@ def main():
 	even_n2 = [[0, -2], [-1, -1], [1, -1], [-2, -1], [2, -1], [-2, 0], [2, 0], [-2, 1], [-1, 2], [0, 2], [1, 2], [2, 1]]
 	odd_n2 = [[0, 2], [-1, 1], [1, 1], [-2, 1], [2, 1], [-2, 0], [2, 0], [-2, -1], [-1, -2], [0, -2], [1, -2], [2, -1]]
 
-	#run board editor
-	editing_board = True
-	while editing_board:
-		editing_board = board_editor(editing_board)
-		draw()
-		pygame.display.flip() # Flip the display so that the things we drew actually show up.
+	global in_menu
+	global in_board_editor
+	global in_game
+	global in_info
 
-	# current player, 1 or 2, 
-	# end of turn: current_player = abs(current_player - 3)
-	current_player = 1
-	selected_tile = None
-	score = get_score()
-	cloneable = []
-	jumpable = []
-	# Main game loop.
-	print("sprites: ", len(group.sprites()))
-	global total_tiles
-	total_tiles = get_total_tiles()
-	print(total_tiles)
+	in_menu = True
+	in_board_editor = False
+	in_game = False
+	in_info = False
+
+	grid, group = create_board()
 	while True:
-		cloneable, jumpable, selected_tile, current_player, score = update(cloneable, jumpable, selected_tile, current_player, score) 
-		draw()
-		draw_outlines(cloneable, jumpable)
-		pygame.display.flip() # Flip the display so that the things we drew actually show up.
+
+		if in_menu: #MENU
+			while in_menu:
+				menu()
+				pygame.display.flip()
+
+		elif in_board_editor: #BOARD EDITOR
+			while in_board_editor:
+				board_editor()
+				draw()
+				pygame.display.flip()
+
+		elif in_game: #MAIN GAMW LOOPs
+			current_player = 1
+			selected_tile = None
+			score = get_score()
+			cloneable = []
+			jumpable = []
+			#print("sprites: ", len(group.sprites()))
+			total_tiles = get_total_tiles()
+			#print(total_tiles)
+			while in_game:
+				cloneable, jumpable, selected_tile, current_player, score = game(cloneable, jumpable, selected_tile, current_player, score) 
+				draw()
+				draw_outlines(cloneable, jumpable)
+				pygame.display.flip()
+
+		elif in_info: #INFO SCREEN --> game rules, credits, NOT IMPLEMENTED, crashes the game currently
+			while in_info:
+				#info()
+				pygame.display.flip()
+				pass
 
 if __name__ == "__main__":
 	main()
